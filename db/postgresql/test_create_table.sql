8b97c272547a8923be5d833f265aca4632c5575a
DKKs <eye.q.21@gmail.com>
2022-03-15 16:46:45 +0700

    [Add] test update script

---
+++ db/postgresql/test_create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE sleep.article (
+    article_id bigserial primary key,
+    article_name varchar(20) NOT NULL,
+    article_desc text NOT NULL,
+    date_added timestamp default NULL
+);
